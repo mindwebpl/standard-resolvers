67a17d2bb0a2b3908090795967f9d863e9e86b8f
Marcin Czołnowski <marcin@czolnowski.net>
2015-04-18 20:34:25 +0200

    Remove from json resolver time attribution.

--- src/Mindweb/StandardResolvers/JsonResolve.php
+++ src/Mindweb/StandardResolvers/JsonResolve.php
@@ -3,37 +3,18 @@ namespace Mindweb\StandardResolvers;
 
 use Mindweb\Resolve\Resolve;
 use Mindweb\Resolve\Event\ResolveEvent;
-use Pimple;
 
 class JsonResolve extends Resolve
 {
-    /**
-     * @var float
-     */
-    private $startTime;
-
-    /**
-     * @param Pimple $application
-     */
-    public function __construct(Pimple $application)
-    {
-        if (isset($application['startTime'])) {
-            $this->startTime = $application['startTime'];
-        }
-    }
     /**
      * @param ResolveEvent $resolveEvent
      */
     public function resolve(ResolveEvent $resolveEvent)
     {
-        $attribution = $resolveEvent->getAttribution();
-
-        if ($this->startTime !== null) {
-            $attribution['executionTime'] = (microtime(true) - $this->startTime) * 1000;
-        }
-
         $resolveEvent->getResponse()->setContent(
-            json_encode($attribution)
+            json_encode(
+                $resolveEvent->getAttribution()
+            )
         );
 
         $resolveEvent->getResponse()->headers->set('Content-Type', 'application/json');
