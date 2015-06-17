a1ab68254af9d9c73a2710ecfa6397ac9fb9802c
Marcin Czołnowski <marcin@czolnowski.net>
2015-06-17 21:15:44 +0200

    Decorate json response with execution time.

--- src/Mindweb/StandardResolvers/JsonResolve.php
+++ src/Mindweb/StandardResolvers/JsonResolve.php
@@ -11,12 +11,23 @@ class JsonResolve extends Resolve
      */
     public function resolve(ResolveEvent $resolveEvent)
     {
+        $attribution = $resolveEvent->getAttribution();
+        $this->decorateAttributionWithExecutionTime($attribution);
+
         $resolveEvent->getResponse()->setContent(
-            json_encode(
-                $resolveEvent->getAttribution()
-            )
+            json_encode($attribution)
         );
 
         $resolveEvent->getResponse()->headers->set('Content-Type', 'application/json');
+    }
+
+    /**
+     * @param array $attribution
+     */
+    private function decorateAttributionWithExecutionTime(array &$attribution)
+    {
+        if (isset($_SERVER['REQUEST_TIME_FLOAT'])) {
+            $attribution['executionTime'] = microtime(true) - $_SERVER['REQUEST_TIME_FLOAT'];
+        }
     }
 }
